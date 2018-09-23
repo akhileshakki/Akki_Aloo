6ac0ab1ec75ae9c01203d4c363d760f8a4589896
Akhilesh <Akki@Akki>
2018-09-23 12:06:07 +0530

    Commit by akki

--- FirstGitProject/src/Demo.java
+++ FirstGitProject/src/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 		System.out.print("Welcome to git");
 		System.out.println("First commit..");
 		System.out.println("second commit..");
+		System.out.println("Last commit...");
 
 	}
 
