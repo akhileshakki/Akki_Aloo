c92159f94e8df8f305928525ae1471b72b9d270e
Alokakumari <43250086+Alokakumari@users.noreply.github.com>
2018-09-23 11:54:56 +0530

    Update Demo.java

--- FirstGitProject/src/Demo.java
+++ FirstGitProject/src/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.print("Welcome to git");
 		System.out.println("First commit..");
-		System.out.println(second commit..");
+		System.out.println("second commit..");
 
 	}
 
